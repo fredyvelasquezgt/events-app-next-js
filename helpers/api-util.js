8e40c35b9378c14bad75b2efed2337f242600daa
Fredy Velasquez <macacoloco696@gmail.com>
2023-05-01 22:57:07 -0600

    created getAllEvents function

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,3 +1,9 @@
+//funcion que me permite fetchear toda mi info desde firbase
+export function getAllEvents() {
+
+}
+
+
 export function getFeaturedEvents() {
     return DUMMY_EVENTS.filter((event) => event.isFeatured);
   }
