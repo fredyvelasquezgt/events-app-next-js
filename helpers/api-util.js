7c2c357f12406bae35a752a6123ab3afc8ec8a56
Fredy Velasquez <macacoloco696@gmail.com>
2023-05-08 23:54:37 -0600

    modified function

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,4 +1,5 @@
 //funcion que me permite fetchear toda mi info desde firbase
+import { getAllEvents } from '../dummy-data';
 export async  function getAllEvents() {
   const response = await fetch('https://nextjs-course-40c91-default-rtdb.firebaseio.com/events.json');
   const data = await response.json()
@@ -21,5 +22,6 @@ export async function getFeaturedEvents() {
   }
 
   export async function getEventById(id) {
-    return DUMMY_EVENTS.find((event) => event.id === id);
+    const allEvents = await getAllEvents();
+    return allEvents.filter((event) => event.isFeatured);
   }
