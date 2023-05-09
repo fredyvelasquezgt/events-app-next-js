f21b8b91b54648193b3e77a328ace2cd68d31d39
Fredy Velasquez <macacoloco696@gmail.com>
2023-05-08 23:35:10 -0600

    pushing data

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -2,6 +2,15 @@
 export async  function getAllEvents() {
   const response = await fetch('https://nextjs-course-40c91-default-rtdb.firebaseio.com/events.json');
   const data = await response.json()
+
+  const events = []
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key]
+    })
+  }
 }
 
 
