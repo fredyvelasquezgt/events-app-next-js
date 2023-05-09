d71490ea43ac39d752a0892529567790f1a2c814
Fredy Velasquez <macacoloco696@gmail.com>
2023-05-08 23:33:27 -0600

    fetching

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,6 +1,6 @@
 //funcion que me permite fetchear toda mi info desde firbase
 export async  function getAllEvents() {
-
+  fetch('https://nextjs-course-40c91-default-rtdb.firebaseio.com/events.json');
 }
 
 
